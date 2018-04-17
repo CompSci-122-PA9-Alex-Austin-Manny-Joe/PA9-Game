fed3be2d47dfd99d6ae66cc752806cf087cf846d
Alex Tuck <alex.tuck@wsu.edu>
2018-04-17 16:06:11 -0700

    SpaceshipChange

--- PA9-GameProject/Spaceship.h
+++ PA9-GameProject/Spaceship.h
@@ -1,1 +1,24 @@
 #pragma once
+#include "laser.h"
+
+
+class Spaceship : public sf::CircleShape
+{
+private:
+	bool shootSound();
+	sf::SoundBuffer laserBuffer;//Sounds can't ever go out of scope. Functions that declare sounds and go
+	sf::Sound laser;               //out of scope do not work.
+
+public:
+	Spaceship(const sf::Color &c,sf::Vector2f &pos, float r) : CircleShape(r, 3)
+	{
+		this->setFillColor(c);
+		this->setPosition(pos);
+		this->rotate(90);	
+	}
+
+	void moveSpaceship(sf::Event pressKey);
+
+	void shootSpaceship(sf::Event pressKey);
+
+};
